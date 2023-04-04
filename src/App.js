4170d379c7468547bd48f5c36f5805b756151eff
ghada <nasrighada10@outlook.com>
2023-04-04 16:50:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import AddTodo from "./component/todo/AddTodo";
+import Navbar from "./component/Navbar";
+import TodoList from "./component/TodoList";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App vh-100">
+      <Navbar />
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-lg-9 col-xl-7">
+          <div class="card rounded-3">
+            <div class="card-body p-4">
+              <AddTodo />
+              <TodoList />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
